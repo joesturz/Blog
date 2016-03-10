a8bb172637983c0dbc3c3e9765678f54dbb5f0e0
Joe Sturzenegger <joesturz84@gmail.com>
2016-03-09 22:28:51 -0700

    now shows the article after creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,33 @@
 class ArticlesController < ApplicationController
-   def new
-   end
-   def create
-      render plain: params[:article].inspect
-   end
+   	def index
+   	end
+
+   	def show
+   		@article = Article.find(params[:id])
+   	end
+
+   	def new
+   	end
+
+   	def edit
+   	end
+
+   	def create
+      	@article = Article.new(article_params)
+
+  		@article.save
+  		redirect_to @article
+   	end
+
+   	def update
+   	end
+
+   	def destroy
+   	end
+
+   	private
+  		def article_params
+    		params.require(:article).permit(:title, :text)
+  		end
+
 end
